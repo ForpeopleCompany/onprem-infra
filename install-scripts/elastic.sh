1c2c2f7e56f93c127e10ad3ab604a1c5201ea8c5
 Michael Gurenkov <m.gurenkov@skillaz.co>
2021-10-05 19:18:48 +0300

    +1

--- install-scripts/elastic.sh
+++ install-scripts/elastic.sh
@@ -3,10 +3,11 @@ helm upgrade elastic \
   --set discovery.type=single-node \
   --namespace elastic \
   --create-namespace \
-  --install
+  --install \
 elastic/elasticsearch
 
 helm upgrade kibana \
    --namespace elastic \
+   --create-namespace \   
    --install \
 elastic/kibana 
