c32563d9d4df649bfd62d4c6a30ccfba928e96f4
 Michael Gurenkov <m.gurenkov@skillaz.co>
2021-10-05 17:43:27 +0300

    +1

--- install/mongodb.sh
+++
@@ -1,8 +0,0 @@
-helm upgrade mongodb \
-  --set auth.username=dbuser \
-  --set auth.password=qE6dP3mK1jX4rB5aH4cY6uP7lF3vV8pN \
-  --set auth.database=orgmanager \
-  --namespace mongodb \
-  --create-namespace \
-  --install \
-bitnami/mongodb
