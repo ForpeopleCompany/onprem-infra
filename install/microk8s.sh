f8357854744a53d1e522d938cb2c882ebb9ab208
 Michael Gurenkov <m.gurenkov@skillaz.co>
2021-10-05 17:29:30 +0300

    +sudo

--- install/microk8s.sh
+++ install/microk8s.sh
@@ -12,11 +12,10 @@ microk8s enable dns storage ingress helm3 host-access
 
 # настройка прав доступа и конфига для kubectl
 sudo usermod -a -G microk8s $USER
-mkdir ~/.kube
 sudo chown -f -R $USER ~/.kube
 sudo microk8s config > ~/.kube/config
 
 # установка и настройка helm
-snap install helm --classic
+sudo snap install helm --classic
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add elastic https://helm.elastic.co
