857954228731ecd57c74e8544755a7f4b99f63d7
 Michael Gurenkov <m.gurenkov@skillaz.co>
2021-10-06 17:59:28 +0300

    +1

--- install-scripts/elastic.sh
+++ install-scripts/elastic.sh
@@ -1,9 +1,9 @@
-helm upgrade elastic \
-  -f values-elastic.yaml \
-  --namespace elastic \
-  --create-namespace \
-  --install \
-elastic/elasticsearch
+# helm upgrade elastic \
+#   -f values-elastic.yaml \
+#   --namespace elastic \
+#   --create-namespace \
+#   --install \
+# elastic/elasticsearch
 
 helm upgrade kibana \
   -f values-kibana.yaml \
