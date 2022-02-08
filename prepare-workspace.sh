7385043caa3f6dcd620ba425e5ae9ebc49d40e11
 Michael Gurenkov <m.gurenkov@skillaz.co>
2022-02-08 14:56:03 +0300

    prepare-workspace

---
+++ prepare-workspace.sh
@@ -0,0 +1,8 @@
+# установка autocomplete для kubectl
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+
+# настройка прав доступа и конфига для kubectl
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
+microk8s config > ~/.kube/config
+newgrp microk8s
