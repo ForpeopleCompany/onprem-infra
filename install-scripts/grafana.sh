4a6bd23626576254bf2943848dd9a1cec72bff7c
 Michael Gurenkov <m.gurenkov@skillaz.co>
2022-02-21 09:25:21 +0300

    grafana

--- install-scripts/grafana.sh
+++ install-scripts/grafana.sh
@@ -1,3 +1,6 @@
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
 helm upgrade --install loki grafana/loki-stack \
   --set fluent-bit.enabled=false,promtail.enabled=false,grafana.enabled=true,prometheus.enabled=true,prometheus.alertmanager.persistentVolume.enabled=false,prometheus.server.persistentVolume.enabled=false \
   -n grafana \
